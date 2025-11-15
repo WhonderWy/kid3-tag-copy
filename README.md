6a1c35b4849c1af815627d18d1ad5398c3bee2f8
William Yin <26218326+WhonderWy@users.noreply.github.com>
2025-11-15 22:03:45 +1100

    Add pipx install support.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Kid3 Tag Copier
+
+A simple tool for copying metadata tags from one audio file to another using Kid3.
+
+## Installation
+
+Ensure `kid3-cli` is installed using apt, pacman, dnf, etc.
+
+```shell
+# Debian-based (Ubuntu-based)
+sudo apt install kid3-cli
+
+# Arch-based
+sudo pacman -S kid3
+paru -S kid3
+
+# Fedora
+sudo dnf install kid3
+```
+
+### pipx
+
+Install using `pipx`:
+
+```shell
+pipx install git+https://github.com/WhonderWy/kid3-tag-copy.git
+```
+
+### AppImage
+
+Download [and `chmod +x`] from Releases.
+
+## Usage
+
+Run without arguments for GUI mode.
+
+```shell
+kid3-tag-copy --src a1.mp3 a2.mp3 a3.mp3 --dst b1.flac b2.flac b3.flac
+```
--- setup.py
+++ setup.py
@@ -2,12 +2,32 @@ from setuptools import setup
 
 setup(
     name="kid3-tag-copy",
-    version="1.0",
-    py_modules=["kid3_tag_copy"],
-    install_requires=["PySide6"],
-    entry_points={
-        "console_scripts": [
-            "kid3-tag-copier=kid3_tag_copy:main",
+    version="1.0.0",
+    license="MIT",
+    license_expression="MIT",
+    description="Copy tags between audio files using Kid3",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="WhonderWy",
+    url="https://github.com/WhonderWy/kid3-tag-copy",
+    packages=["kid3_tag_copy"],
+    scripts=["kid3-tag-copy.py"],
+    install_requires=[
+        "PySide6",
+    ],
+    extras_require={
+        "system": [
+            "kid3-cli",  # System-level dependency that you need to install separately
         ],
     },
+    entry_points={
+        "console_scripts": [
+            "kid3-tag-copy = kid3_tag_copy.kid3_tag_copy:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
